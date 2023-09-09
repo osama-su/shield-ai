0f9c2f14d3d7348faf72109663b42395fb315322
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-09 21:39:29 +0300

    fix import

--- app/Http/Controllers/Api/dashboard/DashboardPageController.php
+++ app/Http/Controllers/Api/dashboard/DashboardPageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePageRequest;
 use App\Http\Requests\UpdatePageRequest;
 use App\Models\Page;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class DashboardPageController extends Controller
