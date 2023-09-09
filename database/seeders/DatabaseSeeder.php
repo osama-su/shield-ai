b356f1e4e9e6f135cdc6535a45f409b662af6d15
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-09 21:29:18 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // admin user
-        \App\Models\User::factory()->firstOrCreate([
+        if (!\App\Models\User::where('email', 'admin@app.com')->exists()){
+             \App\Models\User::factory()->firstOrCreate([
             'name' => 'Admin',
             'email' => 'admin@app.com',
             'password' => 'secret',
         ]);
+        }
+
          $this->call([
              PageSeeder::class,
          ]);
