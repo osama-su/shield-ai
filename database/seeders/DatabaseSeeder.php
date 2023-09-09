b4283b2ec52241598ece0ff930c80cdd90fbdd34
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-09 21:26:38 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // admin user
-        \App\Models\User::factory()->create([
+        \App\Models\User::factory()->firstOrCreate([
             'name' => 'Admin',
             'email' => 'admin@app.com',
             'password' => 'secret',
