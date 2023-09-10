bd1855a39d1c4016b3086272e6d2cde73682cec5
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-10 16:10:33 +0300

    pages seeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PageSeeder extends Seeder
@@ -12,32 +11,88 @@ class PageSeeder extends Seeder
      */
     public function run(): void
     {
-//        \App\Models\Page::factory()->count(5)->create();
+        if (\App\Models\Page::where('slug', 'home')->exists()) {
+            \App\Models\Page::factory()->create([
+                'title' => 'Home',
+                'slug' => 'home',
+                'content' => [
+                    'hero' => [
+                        'title' => 'Welcome to our website!',
+                        'subtitle' => 'This is a subtitle',
+                        'image' => [
+                            'url' => 'https://source.unsplash.com/random/800x600',
+                            'alt' => 'Random image',
+                        ],
+                        'cta' => [
+                            'label' => 'Learn more',
+                            'url' => '#',
+                        ],
+                        'cta2' => [
+                            'label' => 'Contact us',
+                            'url' => '#',
+                        ],
+                    ],
+                    'heading' => [
+                        'title' => 'About us',
+                    ],
+                ],
+            ]);
+        }
 
-        \App\Models\Page::factory()->create([
-            'title' => 'Home',
-            'slug' => 'home',
-            'content' => [
-                'hero' => [
-                    'title' => 'Welcome to our website!',
-                    'subtitle' => 'This is a subtitle',
-                    'image' => [
-                        'url' => 'https://source.unsplash.com/random/800x600',
-                        'alt' => 'Random image',
+        if (\App\Models\Page::where('slug', 'about')->exists()) {
+            \App\Models\Page::factory()->create([
+                'title' => 'Header',
+                'slug' => 'header',
+                'content' => [
+                    'hero' => [
+                        'title' => 'Welcome to our website!',
+                        'subtitle' => 'This is a subtitle',
+                        'image' => [
+                            'url' => 'https://source.unsplash.com/random/800x600',
+                            'alt' => 'Random image',
+                        ],
+                        'cta' => [
+                            'label' => 'Learn more',
+                            'url' => '#',
+                        ],
+                        'cta2' => [
+                            'label' => 'Contact us',
+                            'url' => '#',
+                        ],
                     ],
-                    'cta' => [
-                        'label' => 'Learn more',
-                        'url' => '#',
-                    ],
-                    'cta2' => [
-                        'label' => 'Contact us',
-                        'url' => '#',
+                    'heading' => [
+                        'title' => 'About us',
                     ],
                 ],
-                'heading' => [
-                    'title' => 'About us',
+            ]);
+        }
+
+        if (\App\Models\Page::where('slug', 'about')->exists()) {
+            \App\Models\Page::factory()->create([
+                'title' => 'Footer',
+                'slug' => 'footer',
+                'content' => [
+                    'hero' => [
+                        'title' => 'Welcome to our website!',
+                        'subtitle' => 'This is a subtitle',
+                        'image' => [
+                            'url' => 'https://source.unsplash.com/random/800x600',
+                            'alt' => 'Random image',
+                        ],
+                        'cta' => [
+                            'label' => 'Learn more',
+                            'url' => '#',
+                        ],
+                        'cta2' => [
+                            'label' => 'Contact us',
+                            'url' => '#',
+                        ],
+                    ],
+                    'heading' => [
+                        'title' => 'About us',
+                    ],
                 ],
-            ],
-        ]);
+            ]);
+        }
     }
 }
