9897f55caf4936ee71608d5ddabbe50cef6bd8d4
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-11 21:39:37 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         if (!\App\Models\User::where('email', 'admin@shieldgroup.ae')->exists()){
-             \App\Models\User::factory()->firstOrCreate([
+             \App\Models\User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@shieldgroup.ae',
             'password' => 'secret',
