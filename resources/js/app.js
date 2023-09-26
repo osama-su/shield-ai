60f2f15fb4629f663d25bc394be13f44a6ca70fc
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-09-26 19:14:15 +0300

    prim vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,34 @@
 import './bootstrap';
 import '../css/app.css';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import 'primeicons/primeicons.css';
+import '../css/custom.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
+
+// import as directive
+import BadgeDirective from 'primevue/badgedirective';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue)
+            .use(ToastService)
+            .use(ConfirmationService)
+            .directive('badge', BadgeDirective)
             .mount(el);
     },
     progress: {
